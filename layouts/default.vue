6f007f86497bc89727d6ce19b00c4a8eac8c98ae
Danila Julev <daniljulev@gmail.com>
2022-02-27 16:33:23 +0300

    Movie app build with NuxtJS

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="app">
+    <Nuxt keep-alive></Nuxt>
+  </div>
+</template>
---
+++ pages/movies/_movieid.vue
@@ -0,0 +1,136 @@
+<template>
+  <!-- Loading -->
+  <Loading v-if="$fetchState.pending" />
+  <!-- Movie -->
+  <div v-else class="container single-movie">
+    <NuxtLink class="button" :to="{ name: 'index' }">Back</NuxtLink>
+    <div class="movie-info">
+      <div class="movie-img">
+        <img
+          :src="`https://image.tmdb.org/t/p/w342${movie.poster_path}`"
+          alt=""
+        />
+      </div>
+      <div class="movie-content">
+        <h1>Title: {{ movie.title }}</h1>
+        <p class="movie-fact tagline">
+          <span>Tagline: "{{ movie.tagline }}"</span>
+        </p>
+        <p class="movie-fact">
+          <span>Released:</span>
+          {{
+            new Date(movie.release_date).toLocaleString('en-us', {
+              month: 'long',
+              day: 'numeric',
+              year: 'numeric',
+            })
+          }}
+        </p>
+        <p class="movie-fact">
+          <span>Duration:</span> {{ movie.runtime }} minutes
+        </p>
+        <p class="movie-fact">
+          <span>Revenue:</span>
+          {{
+            movie.revenue.toLocaleString('en-us', {
+              style: 'currency',
+              currency: 'USD',
+            })
+          }}
+        </p>
+        <p class="movie-fact"><span>Overview:</span> {{ movie.overview }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'SingleMovie',
+  data() {
+    return {
+      movie: '',
+    }
+  },
+  async fetch() {
+    await this.getMovie()
+  },
+
+  head() {
+    return {
+      title: this.movie.title,
+    }
+  },
+
+  fetchDelay: 1000,
+
+  methods: {
+    async getMovie() {
+      const responseData = await axios.get(
+        `https://api.themoviedb.org/3/movie/${this.$route.params.movieid}?api_key=07da5581753c3a2cbe717760af2f71ec&language=en-US`
+      )
+      const result = await responseData.data
+      this.movie = result
+    },
+  },
+
+}
+</script>
+
+<style lang="scss">
+.single-movie {
+  color: #fff;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 32px 16px;
+  .button {
+    align-self: flex-start;
+    margin-bottom: 32px;
+  }
+  .movie-info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 32px;
+    color: #fff;
+    @media (min-width: 800px) {
+      flex-direction: row;
+      align-items: flex-start;
+    }
+    .movie-img {
+      img {
+        max-height: 500px;
+        width: 100%;
+        @media (min-width: 800px) {
+          max-height: 700px;
+          width: initial;
+        }
+      }
+    }
+    .movie-content {
+      h1 {
+        font-size: 56px;
+        font-weight: 400;
+      }
+      .movie-fact {
+        margin-top: 12px;
+        font-size: 20px;
+        line-height: 1.5;
+        span {
+          font-weight: 600;
+          text-decoration: underline;
+        }
+      }
+      .tagline {
+        font-style: italic;
+        span {
+          font-style: normal;
+        }
+      }
+    }
+  }
+}
+</style>
